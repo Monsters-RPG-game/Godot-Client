b53c31a80b86aabe83cf5909ba63a9e3e1518d2e
jakub <kiszczyckuba20@wp.pl>
2024-08-04 14:49:01 +0200

    Init

---
+++ characters/player.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D
+
+@export var move_speed : float = 100
+@export var starting_direction : Vector2 = Vector2(0, 1)
+
+@onready var animation_tree = $AnimationTree
+@onready var state_machine = animation_tree.get("parameters/playback")
+
+func _read(): 
+	update_animation_parameters(starting_direction)
+
+func _physics_process(_delta):
+	
+	# Get input direction
+	var input_direction = Vector2(
+		Input.get_action_strength('right') - Input.get_action_strength('left'),
+		Input.get_action_strength('down') - Input.get_action_strength('up')
+		)
+		
+	update_animation_parameters(input_direction)
+
+	# Update velosoty
+	velocity = input_direction * move_speed
+	
+	# Move chracter 
+	move_and_slide()
+	
+	pickk_new_state()
+
+func update_animation_parameters(move_input : Vector2):
+	print('move_input')
+	print(move_input)
+	print("Zero")
+	print(Vector2.ZERO)
+	if(move_input != Vector2.ZERO):
+		animation_tree.set("parameters/Walk/blend_position", move_input)
+		animation_tree.set("parameters/Idle/blend_position", move_input)
+
+func pickk_new_state(): 
+	if(velocity != Vector2.ZERO):
+		state_machine.travel("Walk")
+	else:
+		state_machine.travel("Idle")
