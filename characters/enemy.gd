c033cabb020b105c48960dac2b26893d8b0b5f74
marcin-nedza <nedza.marcin@gmail.com>
2024-08-12 10:04:21 +0200

    added raycast and evading obstacle

--- characters/enemy.gd
+++ characters/enemy.gd
@@ -1,15 +1,19 @@
 extends CharacterBody2D
 
 
-const SPEED = 2000.0
-const JUMP_VELOCITY = -400.0
 @export var hp=100
-var is_dying=false
-
+@export var min_distance_to_player=15
 @onready var player_node:Node2D = get_node("/root/Node2D/Player")  # Adjust path to your scene structure
 @onready var anim_player = $AnimationPlayer  # Assuming the AnimationPlayer is a child of this node
+@onready var raycast = $RayCast2D as RayCast2D 
+
+const SPEED = 2000.0
+const JUMP_VELOCITY = -400.0
+var is_dying=false
 
 func _ready():
+	raycast.enabled = false
+
 	self.add_to_group("depth_sorted")
 	if player_node != null:
 		player_node.connect("player_attack", _on_player_attack)
@@ -21,11 +25,15 @@ func _process(_delta):
 		die()
 		
 func _physics_process(delta):
+	var is_obstacle=check_for_obstacle(player_node)
 	var player_position=player_node.position
 	var target_position=(player_position-position).normalized()
-	if position.distance_to(player_position)>15:
-		velocity=target_position*SPEED*delta
-		move_and_slide()	
+	if position.distance_to(player_position)>min_distance_to_player:
+		var move_direction = target_position
+		if is_obstacle:
+			move_direction =move_direction.rotated(PI/4) 
+		velocity = move_direction * SPEED * delta
+		move_and_slide()
 		
 func _on_player_attack(damage: int) -> void:
 	print("Enemy hit by player with damage: ", damage)
@@ -44,3 +52,8 @@ func die() -> void:
 	anim_player.play("die")
 	await anim_player.animation_finished
 	queue_free()  # Destroys this node
+	
+func check_for_obstacle(target:CharacterBody2D)->bool:
+	raycast.target_position=target.global_position-self.global_position
+	raycast.enabled=true
+	return raycast.is_colliding()
