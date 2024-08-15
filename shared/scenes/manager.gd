f388e83ba29e25e7901cad737c03e138221daa3a
jakub <kiszczyckuba20@wp.pl>
2024-08-15 13:03:21 +0200

    Fix: Added small fix for scene manager not imporing scene state

--- shared/scenes/manager.gd
+++ shared/scenes/manager.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@onready var scene_state = load("res://shared/scenes/state.gd").new()
+
 func switch_scene(change_scene: Callable, old_scene: String, new_scene: String):
-	GlobalSceneState.last_scene = old_scene
+	scene_state.last_scene = old_scene
 	change_scene.call(new_scene)
