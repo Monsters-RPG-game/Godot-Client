71be6367cc4e553ab860719858d1930bbdb7ec32
jakub <kiszczyckuba20@wp.pl>
2024-08-28 20:13:00 +0200

    Feat: Added emitting movement to udp server

--- characters/player.gd
+++ characters/player.gd
@@ -1,6 +1,10 @@
 extends "res://characters/abstract/base_character.gd"
 
+@onready var player_node:Node2D = get_node("/root/Level/Map/Player")
+
 signal player_attack(damage,area_id)
+
+var last_pos: Vector2 = Vector2()
 
 func _ready():
 	update_animation_parameter(starting_direction)
@@ -12,11 +16,12 @@ func _physics_process(delta):
 			Input.get_action_strength("down") - Input.get_action_strength("up")
 		)
 		
-		if not is_attacking:  
+		if not is_attacking:
 			update_animation_parameter(input_direction)
 			velocity = input_direction * SPEED * delta
 			move_and_slide()
-
+		
+		emit_location()
 		handle_attack()
 		pick_new_state()
 
@@ -29,3 +34,8 @@ func _on_sword_hit_area_entered(area:Area2D):
 	if(area.name=='hurtbox'):
 		player_attack.emit(dmg,area.get_instance_id())
 		print("HIT THI",area.get_instance_id())
+
+func emit_location():
+		if(last_pos != player_node.position):
+			last_pos = player_node.position
+			UdpConnection.send_message(JSON.stringify(last_pos))
