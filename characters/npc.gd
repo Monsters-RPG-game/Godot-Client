cace8195e5ddbb725edc43952d43010a91ec6410
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-08-29 17:23:39 +0200

    Feat: Npc following enemy on enetering detection area added

--- characters/npc.gd
+++ characters/npc.gd
@@ -1,5 +1,6 @@
 extends "res://characters/abstract/base_character.gd"
 
+@onready var detection_area: DetectionArea = $DetectionArea
 @onready var interaction_area: InteractionArea = $InteractionArea
 @export var dialogue: DialogueResource
 
@@ -8,8 +9,15 @@ signal npc_attack(damage, area_id)
 func _ready():
 	interaction_area.interact = Callable(self, "_on_interact")
 
-func _physics_process(_delta):
-		pick_new_state()
+func _physics_process(delta):
+	if (detection_area.detected_array.size() > 0 and not is_attacking):
+		var target_position=(detection_area.detected_array[0].position - position).normalized()
+		velocity = target_position * SPEED * delta
+		update_animation_parameter(target_position)
+		move_and_slide()
+	else: 
+		velocity = Vector2.ZERO
+	pick_new_state()
 
 func _on_interact():
 	if (!GameState.in_dialog):
@@ -22,6 +30,7 @@ func _on_sword_hit_area_entered(area:Area2D):
 		npc_attack.emit(dmg,area.get_instance_id())
 		print("HIT THI",area.get_instance_id())
 	
-func _on_attack_detection_area_entered(_area):
+func _on_attack_detection(_area):
 	is_attacking = true
 	attack_timer.start()
+
--- game_componenets/detections/detection_area.gd
+++ game_componenets/detections/detection_area.gd
@@ -1,14 +1,14 @@
 extends Area2D
 class_name DetectionArea
 
-var detected_array: Array = []
+var detected_array: Array[CharacterBody2D] = []
 
-func _on_area_entered(area):
-	print('entered ', detected_array)
-	detected_array.push_back(area)
+func _on_body_entered(body):
+	print("on enter", body)
+	detected_array.push_back(body)
 	
-func _on_area_exited(area):
-	print('areas after exited ', detected_array)
-	var index = detected_array.find(area)
+func _on_body_exited(body):
+	print('on exited', body)
+	var index = detected_array.find(body)
 	if index != -1:
 		detected_array.remove_at(index)
