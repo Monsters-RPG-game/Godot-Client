0b66a6267d4f13d9448bce23d662989c7b596f33
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2024-09-02 23:26:51 +0200

    Feat: chest ui script created, chest ui using player inventory data

--- characters/player.gd
+++ characters/player.gd
@@ -2,14 +2,10 @@ extends BaseCharacter
 
 signal player_attack(damage,area_id)
 
-#@onready var inv_ui = $PlayerInventoryUI
-
 func _ready():
-	#inv_ui.visible = false
 	update_animation_parameter(starting_direction)
 
 func _physics_process(delta): 
-	#handle_inventory()
 	if (!GameState.in_dialog):
 		var input_direction = Vector2(
 			Input.get_action_strength("right") - Input.get_action_strength("left"),
@@ -33,13 +29,3 @@ func _on_sword_hit_area_entered(area:Area2D):
 	if(area.name=='hurtbox'):
 		player_attack.emit(dmg,area.get_instance_id())
 		print("HIT THI",area.get_instance_id())
-
-#func handle_inventory():
-	#if Input.is_action_just_pressed("inventory") and !inv_ui.is_open:
-		#GameState.in_dialog = true
-		#inv_ui.is_open = true
-	#elif Input.is_action_just_pressed("inventory") and inv_ui.is_open:
-		#GameState.in_dialog = false
-		#inv_ui.is_open = false
-		
-		
